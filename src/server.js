f785b504f092bb86ca1e8545883f75140ed7dc63
fran kelly <franclass@MacBook-Air-de-Francielly.local>
2023-10-16 14:37:57 -0300

    chore: setting up mongoose, dotenv and creating database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 import express from "express" 
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+
 import routes from './routes'
+
+dotenv.config()
  
 const server = express(); 
 const port = process.env.PORT || 3000; 
+
+mongoose.connect(`mongodb+srv://francikelly:${process.env.DB_PASSWORD}@cluster0.370tbco.mongodb.net/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true})
 
 server.use(express.json())
 server.use(routes)
